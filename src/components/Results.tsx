796a1376c5727c8271406ad1b3634b7207d418a1
Ivan Kolpakov <vanjaklp95@gmail.com>
2023-04-04 09:15:16 +0300

    add to local storage function

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,8 +1,9 @@
 import { selectSearchResults } from "@/redux/searchSlice"
 import { useDispatch, useSelector } from "react-redux"
 import StarBorderIcon from '@mui/icons-material/StarBorder';
-import { FC } from "react";
-import {addToFavourites} from '../redux/favouritesSlice'
+import StarRateIcon from '@mui/icons-material/StarRate';
+import { FC, useEffect, useState } from "react";
+import {addToFavourites, removeFromFavourites} from '../redux/favouritesSlice'
 import type { FavouriteWord } from "../redux/favouritesSlice";
 import { Definition, Meaning, SearchResult } from "@/types/types";
 
@@ -11,9 +12,39 @@ import { Definition, Meaning, SearchResult } from "@/types/types";
   const searchResults = useSelector(selectSearchResults)
   console.log(searchResults)
 
+  const [favourites, setFavourites] = useState<string[]>([])
+  useEffect(() => {
+    const savedFavourites = localStorage.getItem('favourites');
+    if(savedFavourites) {
+      setFavourites(JSON.parse(savedFavourites))
+    }
+  }, [])
+
+  const isFavourite = (word: FavouriteWord) => {
+    return favourites.includes(word.definition)
+  }
+
+  const addToLocalStorage = (word: FavouriteWord) => {
+    const updatedFavourites = [...favourites, word.definition]
+    localStorage.setItem('favourites', JSON.stringify(updatedFavourites))
+    setFavourites(updatedFavourites);
+  }
+
+  const removeFromLocalStorage = (word: FavouriteWord) => {
+    const updatedFavourites = favourites.filter((fav) => fav !== word.definition)
+    localStorage.setItem('favourites', JSON.stringify(updatedFavourites));
+    setFavourites(updatedFavourites)
+  }
+
   const handleToFavourites = (word: FavouriteWord) => {
+    if(isFavourite(word)) {
+      dispatch(removeFromFavourites(word))
+      removeFromLocalStorage(word)
+    } else {
     console.log(word);
     dispatch(addToFavourites(word))
+    addToLocalStorage(word)
+    }
   }
   return (
     <div className="max-sm:w-full max-sm:justify-center w-2/3">
@@ -29,13 +60,28 @@ import { Definition, Meaning, SearchResult } from "@/types/types";
             <span className="ml-2 truncate overflow-x-hidden">{word.definition}</span>
             </div>
             <div className=" flex justify-end ml-2 ">
-            <span className=" hover:cursor-pointer"><StarBorderIcon onClick={()=>handleToFavourites({
-              definition: word.definition,
-              word: el.word,
-              partOfSpeech: one.partOfSpeech
-            })} sx={{ "&:hover": {
-              color: 'primary.main'
-            }, 'color': 'primary'}}/></span>
+              {isFavourite({
+                definition: word.definition,
+                word: el.word,
+                partOfSpeech: one.partOfSpeech 
+              }) ? (
+                <span className=" hover:cursor-pointer"><StarRateIcon onClick={()=>handleToFavourites({
+                  definition: word.definition,
+                  word: el.word,
+                  partOfSpeech: one.partOfSpeech
+                })} sx={{ "&:hover": {
+                  color: 'primary.main'
+                }, 'color': 'primary.main'}}/></span>
+              ) : (
+                <span className=" hover:cursor-pointer"><StarBorderIcon onClick={()=>handleToFavourites({
+                  definition: word.definition,
+                  word: el.word,
+                  partOfSpeech: one.partOfSpeech
+                })} sx={{ "&:hover": {
+                  color: 'primary.main'
+                }, 'color': 'primary'}}/></span>
+              )}
+            
             </div>
             </div>
             
